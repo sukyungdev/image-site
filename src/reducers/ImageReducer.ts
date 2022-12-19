07d512cb6ac47ca4a233cc9787b252b1513c0e3b
sukyungdev <sukyung.developer@gmail.com>
2022-12-19 22:39:29 +0900

    redux setting

---
+++ src/reducers/ImageReducer.ts
@@ -0,0 +1,11 @@
+interface InitialState {}
+const initialState: InitialState = {};
+
+const ImageReducer = (state = initialState, action: any) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default ImageReducer;
---
+++ src/reducers/index.ts
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import ImageReducer from './ImageReducer';
+
+const rootReducer = combineReducers({
+  ImageReducer,
+});
+
+export default rootReducer;
