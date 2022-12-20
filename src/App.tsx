efab85aa683ee3d3569b789b2e1d6eda4082d2d6
sukyungdev <sukyung.developer@gmail.com>
2022-12-20 12:44:06 +0900

    router setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import FavoritePage from './pages/FavoritePage';
+import MainPage from './pages/MainPage';
 
 function App() {
-  return <div className="App">app</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+      <Route path="/favoritePage" element={<FavoritePage />} />
+    </Routes>
+  );
 }
 
 export default App;
