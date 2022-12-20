bfb0d084706835f99a0cfa7fb0d7a708f895f186
sukyungdev <sukyung.developer@gmail.com>
2022-12-21 01:17:52 +0900

    Add : layout width

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -6,12 +6,18 @@ import Header from '../components/Header';
 
 const Layout = () => {
   return (
-    <>
+    <Container>
       <Header />
       <Outlet />
       <Footer />
-    </>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  width: 100vw;
+  max-width: 1280px;
+  margin: 0 auto;
+`;
 
 export default Layout;
