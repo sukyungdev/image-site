e3f006cb37e21f964e57245fae3ea234fba27a36
sukyungdev <sukyung.developer@gmail.com>
2022-12-21 22:03:49 +0900

    Add : search function
    - Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Header = () => {
-  return <div>Header</div>;
+  const [searchName, setSearchName] = useState('');
+
+  const SearchHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchName(e.target.value);
+    // console.log(searchName);
+  };
+
+  const fetchSearchImageData = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setSearchName('');
+  };
+
+  return (
+    <div>
+      <h1>ImageSite</h1>
+      <form onSubmit={fetchSearchImageData}>
+        <input onChange={SearchHandler} value={searchName} />
+        <button type="submit">search</button>
+      </form>
+    </div>
+  );
 };
 
 export default Header;
