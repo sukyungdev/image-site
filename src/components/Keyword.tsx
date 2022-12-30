1e0f3bacf6e0e7451d2c79d911aa2ac00ea99a9e
sukyungdev <sukyung.developer@gmail.com>
2022-12-31 01:19:44 +0900

    Design : keyword button ui

--- src/components/Keyword.tsx
+++ src/components/Keyword.tsx
@@ -6,7 +6,7 @@ const Keyword = () => {
   return (
     <Container>
       {keywords.map((item, index) => (
-        <button key={index}>{item}</button>
+        <Button key={index}>{item}</Button>
       ))}
     </Container>
   );
@@ -17,9 +17,22 @@ const Container = styled.div`
   max-width: 1280px;
   margin: 1em auto;
   display: flex;
+  flex-wrap: wrap;
   justify-content: center;
   align-items: center;
   gap: 1em;
+`;
+
+const Button = styled.button`
+  width: 100px;
+  padding: 5px 10px;
+  border-radius: 7px;
+  border: 1px solid ${(props) => props.theme.colors.iconColor};
+  box-shadow: rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px;
+  &:hover {
+    color: ${(props) => props.theme.colors.link};
+    border-color: ${(props) => props.theme.colors.link};
+  }
 `;
 
 export default Keyword;
